ffd4091f0a6ba7c489f2abdad5e35e6f0ee0d161
Daniel Passos <daniel@passos.me>
2018-10-15 15:25:04 -0300

    Fetch user data from randomuser.me using redux-thunk

--- redux-offline-example/src/configureStore.js
+++ redux-offline-example/src/configureStore.js
@@ -2,8 +2,9 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import users from "./reducers/usersReducer"
 import {composeWithDevTools} from "redux-devtools-extension";
 import {logger} from "redux-logger";
+import thunk from "redux-thunk"
 
 export const store = createStore(
   combineReducers({users}),
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
