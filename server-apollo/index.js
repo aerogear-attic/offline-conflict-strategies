85996f2f96d32fa42b139410fe41cd30319fc722
Wojciech Trocki <wtrocki@redhat.com>
2018-10-23 14:40:14 +0100

    Split to SDK on the server

--- server-apollo/index.js
+++ server-apollo/index.js
@@ -1,57 +1,46 @@
 const { ApolloServer, gql } = require('apollo-server');
-const { GraphQLError } = require('graphql')
-
-class SyncServerError extends GraphQLError {
-    constructor(message, data, type){
-        const prefix= "AgSync:"
-        super(message)
-        this.type = prefix + type || prefix+ "generic"
-        this.data = data
-        this.version = data.version
-        console.dir(this);
-    }
-}
+const { SyncServerError, detectConflict } = require('./sdk/ConflictSupport')
 
 const feedback = [
-{
-    id: 1,
-    author: 1,
-    text: 'How versioning works with GraphQL?',
-    votes: 7
-},
-{
-    id: 2,
-    author: 2,
-    text: 'Is GraphQL another Hype?',
-    votes: 53
-}
+    {
+        id: 1,
+        author: 1,
+        text: 'How versioning works with GraphQL?',
+        votes: 7
+    },
+    {
+        id: 2,
+        author: 2,
+        text: 'Is GraphQL another Hype?',
+        votes: 53
+    }
 ]
 
 const users = [
-{
-    id: 1,
-    name: 'Stephen',
-    dateOfBirth: '2018-10-15T14:52:31.888Z',
-    version: 1
-},
-{
-    id: 2,
-    name: 'Wojciech',
-    dateOfBirth: '2014-10-15T14:52:38.376Z',
-    version: 1
-},
-{
-    id: 3,
-    name: 'Passos',
-    dateOfBirth: '2018-10-15T17:42:38.376Z',
-    version: 1
-},
-{
-    id: 4,
-    name: 'Dara',
-    dateOfBirth: '2015-10-15T09:51:18.376Z',
-    version: 1
-}
+    {
+        id: 1,
+        name: 'Stephen',
+        dateOfBirth: '2018-10-15T14:52:31.888Z',
+        version: 1
+    },
+    {
+        id: 2,
+        name: 'Wojciech',
+        dateOfBirth: '2014-10-15T14:52:38.376Z',
+        version: 1
+    },
+    {
+        id: 3,
+        name: 'Passos',
+        dateOfBirth: '2018-10-15T17:42:38.376Z',
+        version: 1
+    },
+    {
+        id: 4,
+        name: 'Dara',
+        dateOfBirth: '2015-10-15T09:51:18.376Z',
+        version: 1
+    }
 ]
 
 // Schema
@@ -92,114 +81,110 @@ type Mutation {
 // Resolvers define the technique for fetching the types in the
 // schema.
 const resolvers = {
-Query: {
-    allFeedbacks: () => feedback,
-    allUsers: () => users,
-    getFeedback: (obj, args, context, info) => {
-    for (let value of feedback) {
-        if (args.id == value.id) {
-        return value
-        }
-    }
-    throw new SyncServerError(`Couldn't find feedback with id ${id}`)
-    },
-    getUser: (obj, args, context, info) => {
-    for (let value of users) {
-        if (args.id == value.id) {
-        return value
-        }
-    }
-    throw new SyncServerError(`Couldn't find user with id ${id}`)
-    }
-},
-
-Mutation: {
-    createUser: (obj, args, context, info) => {
-        args.id = new Date().getTime()
-        args.version = 1
-        users.push(args)
-        return args
-    },
-    updateUser: (obj, args, context, info) => {
-        for (let user of users) {
-            if (args.id == user.id) {
-                //if(conflictDetected(user.version, args.version)){
-                    // TODO add some logic to refetch if online
-                    console.warn(`Conflict detected. Server: ${user} client: ${args}`)
-                    throw new SyncServerError("Conflict when saving data",user, "DataConflict")
-                //}
-                // console.log(`Updating user: ${args}`)
-                // user.name = args.name
-                // user.feedback = args.feedback
-                // user.dateOfBirth = args.dateOfBirth
-                // user.version = args.version
-                // return user
+    Query: {
+        allFeedbacks: () => feedback,
+        allUsers: () => users,
+        getFeedback: (obj, args, context, info) => {
+            for (let value of feedback) {
+                if (args.id == value.id) {
+                    return value
+                }
             }
+            throw new SyncServerError(`Couldn't find feedback with id ${id}`)
+        },
+        getUser: (obj, args, context, info) => {
+            for (let value of users) {
+                if (args.id == value.id) {
+                    return value
+                }
+            }
+            throw new SyncServerError(`Couldn't find user with id ${id}`)
         }
     },
-    deleteUser: (obj, args, context, info) => {
-    for (var i = 0; i < users.length; i++) {
-        if (args.id == users[i].id) {
-            let returningUser = users[i]
-            users.splice(i, 1)
-            return returningUser
-        }
-    }
-    throw new Error(`Couldn't find user with id ${args.id}`)
-    },
-    createFeedback: (obj, args, context, info) => {
-        args.id = new Date().getTime()
-        feedback.push(args)
-        return args
-    },
-    updateFeedback: (obj, args, context, info) => {
-    for (let item of feedback) {
-        if (args.id == item.id) {
-            item.votes = args.votes
-            item.author = args.author
-            item.text = args.text
-            return item
-        }
-    }
-    throw new Error(`Couldn't find feedback with id ${args.id}`)
-    },
-    deleteFeedback: (obj, args, context, info) => {
-    for (var i = 0; i < feedback.length; i++) {
-        if (args.id == feedback[i].id) {
-        let returningFeedback = feedback[i]
-        feedback.splice(i, 1)
-        return returningFeedback
-        }
-    }
-    throw new Error(`Couldn't find user with id ${args.id}`)
-    },
-    vote: (obj, args, context, info) => {
-    for (let item of feedback) {
-        if (args.id === item.id) {
-        item.votes += 1
-        return item
-        }
-    }
-    throw new Error(`Couldn't find feedback with id ${args.id}`)
-    }
-}
-}
 
-const conflictDetected = (server, client) => {
-    return server >= client
+    Mutation: {
+        createUser: (obj, args, context, info) => {
+            args.id = new Date().getTime()
+            args.version = 1
+            users.push(args)
+            return args
+        },
+        updateUser: (obj, args, context, info) => {
+            for (let user of users) {
+                if (args.id == user.id) {
+                    const conflict = detectConflict(user, args);
+                    if (conflict) {
+                        console.warn(`Conflict detected. Server: ${user} client: ${args}`)
+                        throw conflict;
+                    }
+                    console.log(`Updating user: ${args}`)
+                    user.name = args.name
+                    user.feedback = args.feedback
+                    user.dateOfBirth = args.dateOfBirth
+                    user.version = args.version
+                    return user
+                }
+            }
+        },
+        deleteUser: (obj, args, context, info) => {
+            for (var i = 0; i < users.length; i++) {
+                if (args.id == users[i].id) {
+                    let returningUser = users[i]
+                    users.splice(i, 1)
+                    return returningUser
+                }
+            }
+            throw new Error(`Couldn't find user with id ${args.id}`)
+        },
+        createFeedback: (obj, args, context, info) => {
+            args.id = new Date().getTime()
+            feedback.push(args)
+            return args
+        },
+        updateFeedback: (obj, args, context, info) => {
+            for (let item of feedback) {
+                if (args.id == item.id) {
+                    item.votes = args.votes
+                    item.author = args.author
+                    item.text = args.text
+                    return item
+                }
+            }
+            throw new Error(`Couldn't find feedback with id ${args.id}`)
+        },
+        deleteFeedback: (obj, args, context, info) => {
+            for (var i = 0; i < feedback.length; i++) {
+                if (args.id == feedback[i].id) {
+                    let returningFeedback = feedback[i]
+                    feedback.splice(i, 1)
+                    return returningFeedback
+                }
+            }
+            throw new Error(`Couldn't find user with id ${args.id}`)
+        },
+        vote: (obj, args, context, info) => {
+            for (let item of feedback) {
+                if (args.id === item.id) {
+                    item.votes += 1
+                    return item
+                }
+            }
+            throw new Error(`Couldn't find feedback with id ${args.id}`)
+        }
+    }
 }
 
 const apolloServer = new ApolloServer({
-typeDefs,
-resolvers,
-playground: {
-    settings: {
-    'editor.theme': 'light',
-    'editor.cursorShape': 'block'
-    },
-    tabs: [{
-    responses: ['{}'],
-    query: `
+    typeDefs,
+    resolvers,
+    playground: {
+        settings: {
+            'editor.theme': 'light',
+            'editor.cursorShape': 'block'
+        },
+        tabs: [{
+            responses: ['{}'],
+            query: `
         query allUsers {
         allUsers{
         id
@@ -261,8 +246,8 @@ playground: {
         }
     }
     `
-    }]
-}
+        }]
+    }
 })
 
 apolloServer.listen().then(({ url }) => {
