edec06e9902f0f9204dcd0602f5972ed6411df8e
Daniel Passos <daniel@passos.me>
2018-10-24 10:23:18 -0300

    Force retry to use increased exponentially time to run instead of random

--- web/src/sdk/links/retryLink.js
+++ web/src/sdk/links/retryLink.js
@@ -4,7 +4,7 @@ import { getOperationDefinition } from "apollo-utilities";
 export const retryOnErrorLink = (numOfAttempts = 5, firstAttempAfter = 2000) => (
   new RetryLink({
 
-    delay: {initial: firstAttempAfter},
+    delay: {initial: firstAttempAfter, jitter: false},
     attempts: (count, operation, error) => {
       const { operation: operationType } = getOperationDefinition(operation.query);
       const isMutation = operationType === 'mutation';
