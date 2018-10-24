8d984c7ff332a33f166f88c10ccd83115398af53
Daniel Passos <daniel@passos.me>
2018-10-24 11:22:23 -0300

    Make sure the retry is not going to fired by query

--- web/src/sdk/links/retryLink.js
+++ web/src/sdk/links/retryLink.js
@@ -3,11 +3,11 @@ import { getOperationDefinition } from "apollo-utilities";
 
 export const retryOnErrorLink = (numOfAttempts = 5, firstAttempAfter = 2000) => (
   new RetryLink({
-
     delay: {initial: firstAttempAfter, jitter: false},
     attempts: (count, operation, error) => {
       const { operation: operationType } = getOperationDefinition(operation.query);
       const isMutation = operationType === 'mutation';
+      if(!isMutation) { return }
       console.info(`retryOnErrorLink: {
         firedAt: ${new Date()},      
         numOfAttempts: ${numOfAttempts}, 
@@ -16,7 +16,7 @@ export const retryOnErrorLink = (numOfAttempts = 5, firstAttempAfter = 2000) => 
         operation: ${operationType},
         error: ${error}
       }`);
-      return (isMutation && (count <= numOfAttempts));
+      return (count <= numOfAttempts);
     }
   })
 );
