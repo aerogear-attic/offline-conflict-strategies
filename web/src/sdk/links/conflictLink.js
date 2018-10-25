163b4c4c31cb45d44938235a6a279602dee8ec02
Stephen Coady <scoady@redhat.com>
2018-10-25 13:03:20 +0100

    change name

--- web/src/sdk/links/conflictLink.js
+++ web/src/sdk/links/conflictLink.js
@@ -1,5 +1,5 @@
 import {onError} from 'apollo-link-error'
-import merge from "deepmerge";
+import diffMergeClientWins from "deepmerge";
 
 export const conflictLink = () => (
   onError(({graphQLErrors, networkError, operation, forward}) => {
@@ -8,9 +8,8 @@ export const conflictLink = () => (
         if(err.extensions && err.extensions.exception){
           switch (err.extensions.exception.type) {
             case 'AgSync:DataConflict':
-              let version = err.extensions.exception.data.version+1
-              let mergedVariables = merge(err.extensions.exception.data, operation.variables)
-              mergedVariables.version = version
+              let mergedVariables = diffMergeClientWins(err.extensions.exception.data, operation.variables)
+              mergedVariables.version = err.extensions.exception.data.version
               operation.variables= mergedVariables
               console.log(`Conflict happened`, operation, err.extensions.exception.data)
               forward(operation)
