95f7ead1ddccd9ea8204063cf8b6d2981ec35528
Daniel Passos <daniel@passos.me>
2018-11-28 20:52:31 -0200

    Add the network status on header

--- web/src/App.js
+++ web/src/App.js
@@ -4,29 +4,29 @@ import {ApolloProvider} from 'react-apollo'
 import {ListUser} from './view/ListUser'
 import {AddUser} from './view/AddUser'
 import {setupApolloClient} from './sdk/apolloClient'
+import NetworkStatus from "./view/NetworkStatus";
 
 class App extends Component {
 
-  state = {apolloClient: null}
+  state = {apolloClient: null};
 
   async componentDidMount() {
-    const apolloClient = await setupApolloClient()
+    const apolloClient = await setupApolloClient();
     this.setState({apolloClient})
   }
 
-
   render() {
 
-    const {apolloClient} = this.state
-    if (!apolloClient) return 'Loading the app'
+    const {apolloClient} = this.state;
+    if (!apolloClient) return 'Loading the app';
 
     return (
       <ApolloProvider client={apolloClient}>
         <React.Fragment>
-
           <div className="App">
             <header className="App-header">
               <h1 className="App-title">AeroGear Sync Offline</h1>
+              <NetworkStatus />
             </header>
           </div>
           <div className="container">
@@ -36,7 +36,6 @@ class App extends Component {
             <div>
             </div>
           </div>
-
         </React.Fragment>
       </ApolloProvider>
     )
